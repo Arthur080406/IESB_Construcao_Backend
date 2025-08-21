bb2343aadf131093c20cb6ccf986b7bb1f80b482
arthurgbabarbosa-png <arthur.gbabarbosa@gmail.com>
2025-08-20 21:29:53 -0300

    Criando primeiro projeto NODE

---
+++ pratica/01-primeiro-projeto/index.js
@@ -0,0 +1,24 @@
+console.log("Rodando meu primeiro projeto no NODE")
+
+let nome = "Arthur"
+
+let sobrenome = "Giovanne"
+
+
+console.log(nome + " " + sobrenome)
+
+
+let idade = 19
+
+console.log("A idade do " + nome + " é " + idade)
+
+
+let exercicios = 0.5
+
+let trabalho = 1.5
+
+let prova = 4.5
+
+let notaCalculada = exercicios + trabalho + prova
+
+console.log("NOTA A1 " + notaCalculada)
